69ef1d7f9df0d4833ad85881407849ffb6a9b8b0
Ricky <ricky.andriamaharo@proximity.fr>
2023-09-25 22:26:41 +0200

    add redirection signup

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -2,7 +2,8 @@ import { handleAuth,handleLogin } from '@auth0/nextjs-auth0';
 
 export const GET = handleAuth({
   signup: handleLogin({ 
-    authorizationParams: { screen_hint: 'signup' } 
+    authorizationParams: { screen_hint: 'signup' } ,
+    returnTo: 'https://www.pampers.com/en-us/registration/thank-you-completed'
   }),
   onError(req: Request, error: Error) {
     console.error(error);
