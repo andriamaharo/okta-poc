300b3fe6b39df1acd72d03ff3eccdc6a90192e07
Ricky <ricky.andriamaharo@proximity.fr>
2023-10-25 18:23:53 +0200

    add test email

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -2,7 +2,7 @@ import { handleAuth,handleLogin } from '@auth0/nextjs-auth0';
 
 export async function  GET(req : Request) {
   var defaultEmail = req.url.includes("signup-tools") ? 'ricky.andriamaharo@proximity.fr' : '';
-  handleAuth({
+ return handleAuth({
     signup: handleLogin({ 
       authorizationParams: { screen_hint: 'signup', login_hint: defaultEmail  } ,
       returnTo: 'https://www.pampers.com/en-us/registration/thank-you-completed'
