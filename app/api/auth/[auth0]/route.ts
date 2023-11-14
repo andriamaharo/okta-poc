7c3147296eb36c87246274a1d0f4b4e50fbbdf40
Ricky <ricky.andriamaharo@proximity.fr>
2023-11-14 12:12:55 +0100

    add local

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -2,7 +2,7 @@ import { handleAuth,handleLogin } from '@auth0/nextjs-auth0';
 
 export const GET = handleAuth({
   signup: handleLogin({ 
-    authorizationParams: { screen_hint: 'signup',ui_locales:"en-US"} ,
+    authorizationParams: { screen_hint: 'signup',ui_locales:"en-US", login_hint: "test@gmail.com"} ,
     returnTo: 'https://www.pampers.com/en-us/registration/thank-you-completed'
   }),
   login : handleLogin({}),
