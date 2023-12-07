21334ce03cb4e6ff3558a54ee5560d06ba91b5c7
Ricky <ricky.andriamaharo@proximity.fr>
2023-12-07 15:06:36 +0100

    add access token

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -1,10 +1,11 @@
 import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0';
+import { getAccessToken } from '@auth0/nextjs-auth0';
 import { NextResponse } from 'next/server';
 
 const GET = withApiAuthRequired(async () => {
   const session = await getSession();
-
-  return NextResponse.json(session?.user);
+  const { accessToken } = await getAccessToken();
+  return NextResponse.json(accessToken);
 });
 
 export { GET };
