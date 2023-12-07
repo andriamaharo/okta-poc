d9833145be0ec2a859735f79454751bc2470f688
Ricky <ricky.andriamaharo@proximity.fr>
2023-12-07 15:26:22 +0100

    test logs error

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { getSession, withPageAuthRequired } from '@auth0/nextjs-auth0';
 import ServerComponent from '@/app/server-component';
 import ClientComponent from '@/app/client-component';
+import { getAccessToken } from '@auth0/nextjs-auth0';
 
 export default withPageAuthRequired(
   async function Page() {
     const session = await getSession();
+    const { accessToken } = await getAccessToken();
 
     return (
       <main>
         <h1>Profile</h1>
         <h2>Page:</h2>
         <h3>Access Token</h3>
-        <pre>{JSON.stringify({ accessToken: session?.accessToken }, null, 2)}</pre>
-        <h3>User</h3>
+        <pre>{JSON.stringify({ accessToken }, null, 2)}</pre>
+        <h3>User session</h3>
         <pre>{JSON.stringify(session?.user, null, 2)}</pre>
         <h2>Server Component:</h2>
         <ServerComponent />
